9130960233fc01b8947a2c1a06a7bbdedca193ea
jlambert12013 <jlambert12013@gmail.com>
2022-05-19 02:17:02 -0500

    This version is updated with user auth. May 17, 2022 at 2:16am.

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,13 +1,55 @@
-const loginUser = (req, res) => {
+// const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/User");
+
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Please Include All Fields.");
+  }
+
+  // check to see if the users exists
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("Email Already Registered.");
+  }
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid User Data.");
+  }
+
+  res.json({ message: "User Registered" });
+});
+
+const loginUser = asyncHandler(async (req, res) => {
   res.json({ message: "Login User" });
-};
+});
 
-const getUser = (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
   res.json({ message: "User Data Display" });
-};
+});
 
-const registerUser = (req, res) => {
-  res.json({ message: "Register User" });
-};
-
-module.exports = { loginUser, getUser, registerUser };
+module.exports = { registerUser, getUser, loginUser };
